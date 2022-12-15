a3a264722d338bf794b695f18303a9e756511104
Miguel Salinas Lueyza <miguel.salinas@puntonet.cl>
2022-12-15 11:58:35 -0300

    Se agrega hora

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine("Ingresar nombre");
 string nombre = Console.ReadLine();
 
 Console.WriteLine("Hola, " + nombre);
+
+string hora = System.DateTime.Now.Hour + ":" + System.DateTime.Now.Minute;
+
+Console.WriteLine(hora);
